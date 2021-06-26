f0462955da6aed0e78452a1507c6484733243e5a
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-06-26 15:39:17 -0300

    add DeliveryPointsList

--- src/components/CRUDS/DeliveryPoints/DeliveryPointsList/index.tsx
+++ src/components/CRUDS/DeliveryPoints/DeliveryPointsList/index.tsx
@@ -1,23 +1,64 @@
+import { Theme, useMediaQuery } from '@material-ui/core'
 import {
   Datagrid,
   List,
   NumberField,
   TextField,
-  EditButton
+  EditButton,
+  BulkDeleteButton,
+  BulkExportButton,
+  ShowButton,
+  SimpleList
 } from 'ra-ui-materialui'
 import * as React from 'react'
+import { cloneElement, memo } from 'react'
+
+import { DeliveryPointsListActions } from './styles'
+
+const DeliveryPointsActionToolbar = ({ children, ...props }) => {
+  return (
+    <DeliveryPointsListActions>
+      {children.map(children, button => cloneElement(button, props))}
+    </DeliveryPointsListActions>
+  )
+}
+
+const DeliveryPointsListBulkActions = memo(({ children, ...props }) => (
+  <div>
+    <BulkDeleteButton {...props} />
+    <BulkExportButton {...props} />
+  </div>
+))
 
 const DeliveryPointsList: React.FC = props => {
+  const isSmall = useMediaQuery<Theme>(theme => theme.breakpoints.down('sm'))
+
   return (
     <List {...props}>
-      <Datagrid>
-        <TextField source="city" />
-        <TextField source="state" />
-        <TextField source="suburb" />
-        <TextField source="street" />
-        <NumberField source="number" />
-        <TextField source="cep" />
-      </Datagrid>
+      {isSmall ? (
+        <SimpleList
+          primaryText={record => record.name}
+          tertiaryText={record => record.category}
+        />
+      ) : (
+        <Datagrid>
+          <TextField
+            source="city"
+            label="Cidade"
+            sortBy="city.city"
+            sortable={false}
+          />
+          <TextField source="state" label="Estado" sortable={false} />
+          <TextField source="suburb" label="Bairro" sortable={false} />
+          <TextField source="street" label="Rua" sortable={false} />
+          <NumberField source="number" label="Número" sortable={false} />
+          <TextField source="cep" label="CEP" sortable={false} />
+          <DeliveryPointsActionToolbar>
+            <EditButton />
+            <ShowButton />
+          </DeliveryPointsActionToolbar>
+        </Datagrid>
+      )}
     </List>
   )
 }
---
+++ src/components/CRUDS/DeliveryPoints/DeliveryPointsList/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const DeliveryPointsListActions = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: -1;
+  margin-bottom: -1;
+`
