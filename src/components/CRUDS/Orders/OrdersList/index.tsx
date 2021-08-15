9c7f2d5b55a52e3dce4cd75fbaca30020ea54b09
Proyol <danielfavero17@gmail.com>
2021-08-15 19:19:00 -0300

    resolve

--- src/components/CRUDS/Orders/OrdersList/index.tsx
+++ src/components/CRUDS/Orders/OrdersList/index.tsx
@@ -84,6 +84,27 @@ const OrdersFilter = props => {
   )
 }
 
+// in PostList.js
+import { downloadCSV } from 'react-admin'
+import jsonExport from 'jsonexport/dist'
+
+const exporter = posts => {
+  const postsForExport = posts.map(post => {
+    const { backlinks, author, ...postForExport } = post // omit backlinks and author
+    postForExport.author_name = post.author.name // add a field
+    return postForExport
+  })
+  jsonExport(
+    postsForExport,
+    {
+      headers: ['id', 'title', 'author_name', 'body'] // order fields in the export
+    },
+    (err, csv) => {
+      downloadCSV(csv, 'posts') // download as 'posts.csv` file
+    }
+  )
+}
+
 const OrdersList: React.FC<ListProps> = props => {
   const isSmall = useMediaQuery<Theme>(theme => theme.breakpoints.down('sm'))
 
